82689bb8b1d2ae0124aea507963112184bc9c7d7
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-12-29 16:28:46 +0630

    exps

---
+++ experiments/js/Vec.ts
@@ -0,0 +1,4 @@
+export default class Vec {
+    constructor(public x: number, public y: number){
+    }
+}
---
+++ experiments/js/main.ts
@@ -0,0 +1,5 @@
+import Vec from "./Vec";
+
+const location = new Vec(32,33);
+
+console.log(location);
