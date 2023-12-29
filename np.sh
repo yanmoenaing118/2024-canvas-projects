81e2e9e084613142f62069dc1e4092b73ad06290
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-12-29 16:17:05 +0630

    config ts

--- np.sh
+++ np.sh
@@ -5,6 +5,7 @@ cp webpack.config.js $projectName
 cp package.json $projectName
 cp .gitignore
 cp template.html $projectName
+cp tsconfig.json $projectName
 cd $projectName
 mv template.html index.html
 npm install webpack webpack-cli webpack-dev-server typescript ts-loader --save-dev
